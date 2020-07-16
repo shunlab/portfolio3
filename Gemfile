c29df970904708940b324806043e848f2e7f3812
shunlab <0kk4024938z195v@au.com>
2020-07-16 11:52:31 +0900

    画像投稿の作成

--- Gemfile
+++ Gemfile
@@ -10,6 +10,8 @@ gem 'turbolinks',   '5.0.1'
 gem 'uglifier',     '3.2.0'
 gem 'will_paginate',           '3.1.7'
 gem 'bootstrap-will_paginate', '1.0.0'
+gem 'carrierwave',             '1.2.2'
+gem "mini_magick",             '>= 4.9.4'
 
 group :development, :test do
   gem 'byebug', '9.0.6', platform: :mri
@@ -39,6 +41,7 @@ end
 
 group :production do
   gem 'pg', '0.20.0'
+  gem 'fog', '1.42' 
 end
 gem 'devise'
 gem 'bootstrap-sass'
--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -44,12 +44,14 @@ class DishesController < ApplicationController
      redirect_to root_url
    end
  end
- 
+ def dish_params
+   params.require(:dish).permit(:name, :description, :portion, :tips,
+                                :reference, :required_time, :popularity, :cook_memo, :picture)
+ end
+
   private
 
-    def dish_params
-      params.require(:dish).permit(:name, :discription, :portion, :tips, :reference, :required_time, :popularity, :cook_memo)
-    end
+    
   def correct_user
     # 現在のユーザーが更新対象の料理を保有しているかどうか確認
     @dish = current_user.dishes.find_by(id: params[:id])
