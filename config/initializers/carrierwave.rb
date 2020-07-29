8ba931be98bbc487c027fb5aeaa74ffa3a1a5e00
shunlab <0kk4024938z195v@au.com>
2020-07-29 15:23:49 +0900

    S3調整

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,25 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'sakaishunsuke'
+-    config.asset_host = 'https://s3.amazonaws.com/sakaishunsuke'
++    config.asset_host = 'https://sakaishunsuke.s3.amazonaws.com'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['S3_ACCESS_KEY'],
+      aws_secret_access_key: ENV['S3_SECRET_KEY'],
+      region: ENV['S3_REGION'],
+      path_style: true
+    }
+  else
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
+  end
+end
+
+CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
